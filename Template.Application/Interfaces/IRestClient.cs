75766c6517c88e4af7f9c2ec6c51c9dde79be309
Ronald Bett <engkipkemoi@gmail.com>
2023-05-09 10:04:39 +0300

    chore: error updates

--- Template.Application/Interfaces/IRestClient.cs
+++ Template.Application/Interfaces/IRestClient.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace ScvAgritech.Onboarding.Application.Interfaces;
+namespace Template.Application.Interfaces;
 
 public interface IRestClient
 {
